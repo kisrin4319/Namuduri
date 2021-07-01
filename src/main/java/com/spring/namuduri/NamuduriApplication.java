9a98591f22dc8b922cc2054c4b4ebb820a74c1e8
kisrin4319 <kisrin4319@Naver.com>
2021-07-01 09:51:40 +0900

    Initial commit

---
+++ src/main/java/com/spring/namuduri/NamuduriApplication.java
@@ -0,0 +1,13 @@
+package com.spring.namuduri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamuduriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamuduriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/namuduri/NamuduriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.namuduri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamuduriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
